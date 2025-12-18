8ffb42810cea43a64e987471e7efeec394b43cf1
vadherjwellery-lgtm <YOUR_GITHUB_EMAIL>
2025-12-19 01:49:19 +0530

    okay

--- src/config/passport.js
+++ src/config/passport.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://web1-backend-zjrb.onrender.com/auth/google/callback",
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
